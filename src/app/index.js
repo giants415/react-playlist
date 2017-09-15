e3bc2b9a05527e5459d0189b78a0cb138062d6ba
Andrew <andrew.vinocur@gmail.com>
2017-09-15 15:45:56 -0700

    introduces state and finishes video 5

--- src/app/index.js
+++ src/app/index.js
@@ -3,15 +3,24 @@ var ReactDOM = require('react-dom');
 
 //Create component
 var TodoComponent = React.createClass({
+  getInitialState: function(){
+    return {
+      todos:['wash up', 'eat cheese', 'walk mark sanchez']
+    }
+  },
   render: function(){
     return(
-      <div>
-        <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
-        <p><strong>Cheese Smell factor:</strong> {this.props.cheese.smellFactor}</p>
-        <p><strong>Cheese price:</strong> {this.props.cheese.price}</p>
+      <div id="todo-list">
+        <p>The busiest people have the most leisure</p>
+        <ul>
+          <li>{this.state.todos[0]}</li>
+          <li>{this.state.todos[1]}</li>
+          <li>{this.state.todos[2]}</li>
+        </ul>
+        <ListComponent todos={this.state.todos}/>
       </div>
     );
-  }
+  }// render
 });
 
 var myCheese = {
@@ -22,4 +31,4 @@ var myCheese = {
 
 
 //put component into HTML page
-ReactDOM.render(<TodoComponent mssg="I like cookies" cheese={myCheese}/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
