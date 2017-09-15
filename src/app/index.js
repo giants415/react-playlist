068c92db2081fa8fa39fc27487cd424d9c1d3dda
Andrew <andrew.vinocur@gmail.com>
2017-09-15 15:30:42 -0700

    expands on first component w/ props and finished vid 4

--- src/app/index.js
+++ src/app/index.js
@@ -5,11 +5,21 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   render: function(){
     return(
-      <h1>Vinocur rules!</h1>
+      <div>
+        <p><strong>Cheese name:</strong> {this.props.cheese.name}</p>
+        <p><strong>Cheese Smell factor:</strong> {this.props.cheese.smellFactor}</p>
+        <p><strong>Cheese price:</strong> {this.props.cheese.price}</p>
+      </div>
     );
   }
 });
 
+var myCheese = {
+  name: 'Monterey Jack',
+  smellFactor: "not very",
+  price: '4.00'
+}
+
 
 //put component into HTML page
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent mssg="I like cookies" cheese={myCheese}/>, document.getElementById('todo-wrapper'));
