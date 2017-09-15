2d50663981fd49fb359e07ccfea33eb35de7130d
Andrew <andrew.vinocur@gmail.com>
2017-09-15 15:57:53 -0700

    finishes video 5.5

--- src/app/index.js
+++ src/app/index.js
@@ -5,19 +5,25 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   getInitialState: function(){
     return {
-      todos:['wash up', 'eat cheese', 'walk mark sanchez']
+      todos:['wash up', 'eat cheese', 'walk mark sanchez'],
+      age: 27
     }
   },
   render: function(){
+    var ager = setTimeout(function(){
+      this.setState({
+        age: 37
+      });
+    }.bind(this), 5000);
     return(
       <div id="todo-list">
         <p>The busiest people have the most leisure</p>
+        <p>{this.state.age}</p>
         <ul>
           <li>{this.state.todos[0]}</li>
           <li>{this.state.todos[1]}</li>
           <li>{this.state.todos[2]}</li>
         </ul>
-        <ListComponent todos={this.state.todos}/>
       </div>
     );
   }// render
